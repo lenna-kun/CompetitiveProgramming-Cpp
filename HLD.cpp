90f692e1bad199a483f06267ab72df13eb6afbe5
lenna_kun <lennakun2469@gmail.com>
2021-12-05 10:07:46 +0900

    edit HLD

--- HLD.cpp
+++ HLD.cpp
@@ -4,6 +4,12 @@ using namespace std;
 using namespace atcoder;
 template <class T> bool chmax(T& a, T b){ if(a < b){ a = b; return 1; } return 0; }
 
+class Node {
+public:
+  int depth, parent, top, indexf, indexb;
+  Node() : parent(-1) {}
+};
+
 template <class S, S (*op)(S, S), S (*e)()> class HLD {
 public:
   HLD() : HLD(vector<vector<int>>(0), -1) {}
@@ -23,68 +29,62 @@ public:
   }
   // set element to node
   void set(int node, S x) {
-    stf.set(node_p[node].first, x);
-    stb.set(node_p[node].second, x);
+    stf.set(nodes[node].indexf, x);
+    stb.set(nodes[node].indexb, x);
   }
   // lca
   int lca(int ln, int rn) {
     while (1) {
-      int topl = top[ln], topr = top[rn];
+      int topl = nodes[ln].top, topr = nodes[rn].top;
       if (topl == topr) {
-        if (depth[ln] < depth[rn]) return ln;
+        if (nodes[ln].depth < nodes[rn].depth) return ln;
         else return rn;
       }
-      if (depth[topl] > depth[topr]) ln = parent[topl];
-      else rn = parent[topr];
+      if (nodes[topl].depth > nodes[topr].depth) ln = nodes[topl].parent;
+      else rn = nodes[topr].parent;
     }
   }
   // path query
   S query(int ln, int rn) {
     S prodl = e(), prodr = e();
     while (1) {
-      int topl = top[ln], topr = top[rn];
+      int topl = nodes[ln].top, topr = nodes[rn].top;
       if (topl == topr) {
-        if (node_p[ln] < node_p[rn]) { // rn is deeper than ln
-          prodl = op(prodl, stf.prod(node_p[ln].first, node_p[rn].first+1));
+        if (nodes[ln].depth < nodes[rn].depth) { // rn is deeper than ln
+          prodl = op(prodl, stf.prod(nodes[ln].indexf, nodes[rn].indexf+1));
         } else { // ln is deeper than rn
-          prodl = op(prodl, stb.prod(node_p[ln].second, node_p[rn].second+1));
+          prodl = op(prodl, stb.prod(nodes[ln].indexb, nodes[rn].indexb+1));
         }
         return op(prodl, prodr);
       }
-      if (depth[topl] > depth[topr]) {
-        prodl = op(prodl, stb.prod(node_p[ln].second, node_p[topl].second+1));
-        ln = parent[topl];
+      if (nodes[topl].depth > nodes[topr].depth) {
+        prodl = op(prodl, stb.prod(nodes[ln].indexb, nodes[topl].indexb+1));
+        ln = nodes[topl].parent;
       } else {
-        prodr = op(stf.prod(node_p[topr].first, node_p[rn].first+1), prodr);
-        rn = parent[topr];
+        prodr = op(stf.prod(nodes[topr].indexf, nodes[rn].indexf+1), prodr);
+        rn = nodes[topr].parent;
       }
     }
   }
 private:
   int n, root;
   segtree<S, op, e> stf, stb;
-  vector<int> parent, top, depth;
-  vector<pair<int, int>> node_p;
+  vector<Node> nodes;
   void build_(vector<vector<int>> &tree, const vector<S> &v) {
     assert(n == int(v.size())); assert(root < n);
     // create segtree
     stf = segtree<S, op, e>(n);
     stb = segtree<S, op, e>(n);
 
-    parent = vector<int>(n);
-    top = vector<int>(n);
-    node_p = vector<pair<int, int>>(n);
-    depth = vector<int>(n);
+    nodes = vector<Node>(n);
     vector<int> heavy(n, -1);
     preprocess1(tree, heavy, root, root, 0);
-    parent[root] = -1;
-    int tour_index = 0;
-    preprocess2(tree, v, heavy, tour_index, root, root, root);
+    preprocess2(tree, v, heavy, 0, root, root, root);
   }
   // set heavy node, parent, depth
   int preprocess1(vector<vector<int>> &tree, vector<int> &heavy, int node, int p, int d) {
-    parent[node] = p;
-    depth[node] = d;
+    nodes[node].parent = p;
+    nodes[node].depth = d;
     int size = 1, size_max = 0, size_c;
     for (int &c: tree[node]) {
       if (c == p) continue;
@@ -96,11 +96,11 @@ private:
   }
   // build segtree, set top
   int preprocess2(vector<vector<int>> &tree, const vector<S> &v, vector<int> &heavy, int tour_index, int node, int p, int t) {
-    node_p[node].first = tour_index;
+    nodes[node].indexf = tour_index;
     stf.set(tour_index++, v[node]);
-    node_p[node].second = n - tour_index;
+    nodes[node].indexb = n - tour_index;
     stb.set(n - tour_index, v[node]);
-    top[node] = t; // set top
+    nodes[node].top = t; // set top
     if (heavy[node] >= 0) { // heavy node and depth first search
       tour_index = preprocess2(tree, v, heavy, tour_index, heavy[node], node, t);
     }
