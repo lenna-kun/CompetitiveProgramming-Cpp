f933a751368f9bef99cf45c24455da2bac932c78
lenna_kun <lennakun2469@gmail.com>
2022-01-16 22:18:56 +0900

    minor fixes

--- HLD.cpp
+++ HLD.cpp
@@ -6,7 +6,7 @@ template <class T> bool chmax(T& a, T b){ if(a < b){ a = b; return 1; } return 0
 
 class Node {
 public:
-  int depth, parent, top, indexf, indexb;
+  int depth, parent, top, idxf, idxb;
   Node() : parent(-1) {}
 };
 
@@ -20,6 +20,7 @@ public:
     build_(tree, v);
   }
   void build(vector<vector<int>> &tree, int root_) {
+    n = int(tree.size());
     build(tree, root_, vector<S>(n, e()));
   }
   void build(vector<vector<int>> &tree, int root_, const vector<S> &v) {
@@ -29,8 +30,8 @@ public:
   }
   // set element to node
   void set(int node, S x) {
-    stf.set(nodes[node].indexf, x);
-    stb.set(nodes[node].indexb, x);
+    stf.set(nodes[node].idxf, x);
+    stb.set(nodes[node].idxb, x);
   }
   // lca
   int lca(int ln, int rn) {
@@ -51,17 +52,17 @@ public:
       int topl = nodes[ln].top, topr = nodes[rn].top;
       if (topl == topr) {
         if (nodes[ln].depth < nodes[rn].depth) { // rn is deeper than ln
-          prodl = op(prodl, stf.prod(nodes[ln].indexf, nodes[rn].indexf+1));
+          prodl = op(prodl, stf.prod(nodes[ln].idxf, nodes[rn].idxf+1));
         } else { // ln is deeper than rn
-          prodl = op(prodl, stb.prod(nodes[ln].indexb, nodes[rn].indexb+1));
+          prodl = op(prodl, stb.prod(nodes[ln].idxb, nodes[rn].idxb+1));
         }
         return op(prodl, prodr);
       }
       if (nodes[topl].depth > nodes[topr].depth) {
-        prodl = op(prodl, stb.prod(nodes[ln].indexb, nodes[topl].indexb+1));
+        prodl = op(prodl, stb.prod(nodes[ln].idxb, nodes[topl].idxb+1));
         ln = nodes[topl].parent;
       } else {
-        prodr = op(stf.prod(nodes[topr].indexf, nodes[rn].indexf+1), prodr);
+        prodr = op(stf.prod(nodes[topr].idxf, nodes[rn].idxf+1), prodr);
         rn = nodes[topr].parent;
       }
     }
@@ -78,36 +79,36 @@ private:
 
     nodes = vector<Node>(n);
     vector<int> heavy(n, -1);
-    preprocess1(tree, heavy, root, root, 0);
-    preprocess2(tree, v, heavy, 0, root, root, root);
+    pp1(tree, heavy, root, root, 0);
+    pp2(tree, v, heavy, 0, root, root, root);
   }
   // set heavy node, parent, depth
-  int preprocess1(vector<vector<int>> &tree, vector<int> &heavy, int node, int p, int d) {
+  int pp1(vector<vector<int>> &tree, vector<int> &heavy, int node, int p, int d) {
     nodes[node].parent = p;
     nodes[node].depth = d;
-    int size = 1, size_max = 0, size_c;
+    int sz = 1, sz_max = 0, sz_c;
     for (int &c: tree[node]) {
       if (c == p) continue;
-      size_c = preprocess1(tree, heavy, c, node, d+1); // get child's size
-      size += size_c; // update size
-      if (chmax(size_max, size_c)) heavy[node] = c; // update heavy node
+      sz_c = pp1(tree, heavy, c, node, d+1); // get child's size
+      sz += sz_c; // update size
+      if (chmax(sz_max, sz_c)) heavy[node] = c; // update heavy node
     }
-    return size;
+    return sz;
   }
   // build segtree, set top
-  int preprocess2(vector<vector<int>> &tree, const vector<S> &v, vector<int> &heavy, int tour_index, int node, int p, int t) {
-    nodes[node].indexf = tour_index;
-    stf.set(tour_index++, v[node]);
-    nodes[node].indexb = n - tour_index;
-    stb.set(n - tour_index, v[node]);
+  int pp2(vector<vector<int>> &tree, const vector<S> &v, vector<int> &heavy, int tour_idx, int node, int p, int t) {
+    nodes[node].idxf = tour_idx;
+    stf.set(tour_idx++, v[node]);
+    nodes[node].idxb = n - tour_idx;
+    stb.set(n - tour_idx, v[node]);
     nodes[node].top = t; // set top
     if (heavy[node] >= 0) { // heavy node and depth first search
-      tour_index = preprocess2(tree, v, heavy, tour_index, heavy[node], node, t);
+      tour_idx = pp2(tree, v, heavy, tour_idx, heavy[node], node, t);
     }
     for (int &c: tree[node]) {
       if (c == p || c == heavy[node]) continue;
-      tour_index = preprocess2(tree, v, heavy, tour_index, c, node, c); // next node is top
+      tour_idx = pp2(tree, v, heavy, tour_idx, c, node, c); // next node is top
     }
-    return tour_index;
+    return tour_idx;
   }
 };
