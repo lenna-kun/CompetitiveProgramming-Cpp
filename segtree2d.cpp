c17c254cc05344267f3f6b9e6f141388b98506f9
lenna_kun <lennakun2469@gmail.com>
2022-01-19 13:14:38 +0900

    implove execution time performance

--- segtree2d.cpp
+++ segtree2d.cpp
@@ -18,47 +18,62 @@ i64 op(i64 a, i64 b) { return a + b; }
 
 template <class S, S (*op)(S, S), S (*e)()> class SegTree2d {
 public:
-  void init(vector<vector<int>> &v) {
-    int n = v.size();
-    size = 1; while (size < n) size *= 2;
-    index.resize(2 * size);
-    rep (n) for (int &j: v[i]) index[i + size].push_back(j);
-    rrep (i, 1, size * 2) {
-      sort(all(index[i]));
-      index[i].erase(unique(all(index[i])), index[i].end());
-      for (int &j: index[i]) index[i / 2].push_back(j);
-    }
-    st.resize(2 * size);
-    rep (i, 1, size * 2) st[i] = segtree<S, op, e>(index[i].size());
+  void add_point(i64 x, i64 y) {
+    pts[x].push_back(y);
+    pt.push_back(x);
   }
-  void set(int x, int y, S v) { update(x, y, v, true); }
-  void add(int x, int y, S v) { update(x, y, v, false); }
-  S prod(int sx, int sy, int tx, int ty) {
+  void init() {
+    sort(all(pt));
+    pt.erase(unique(all(pt)), pt.end());
+
+    int n = pt.size();
+    size = 1; while (size < n) size *= 2;
+    idx.resize(2*size);
+
+    int i = 0;
+    for (auto &[x, ys]: pts) idx[(i++)+size] = ys;
+    // pts.clear();
+    rrep (i, 1, 2*size) {
+      sort(all(idx[i]));
+      idx[i].erase(unique(all(idx[i])), idx[i].end());
+      for (i64 &j: idx[i]) idx[i / 2].push_back(j);
+    }
+    st.resize(2*size);
+    rep (i, 1, 2*size) st[i] = segtree<S, op, e>(idx[i].size());
+  }
+  void set(i64 x, i64 y, S v) { update(x, y, v, true); }
+  void add(i64 x, i64 y, S v) { update(x, y, v, false); }
+  S prod(i64 sx, i64 sy, i64 tx, i64 ty) {
     return prod(sx, tx, sy, ty, 1, 0, size);
   }
 private:
   int size;
+  map<i64, vector<i64>> pts;
+  vector<i64> pt;
   vector<segtree<S, op, e>> st;
-  vector<vector<int>> index;
-  void update(int x, int y, S v, bool mode) {
-    assert(x < size);
-    x += size;
-    while (x) {
-      int yy = lower_bound(all(index[x]), y) - index[x].begin();
-      assert(yy != index[x].size());
-      assert(y == index[x][yy]);
-      if (mode) st[x].set(yy, v);
-      else st[x].set(yy, st[x].get(yy) + v);
-      x >>= 1;
+  vector<vector<i64>> idx;
+  void update(i64 x, i64 y, S v, bool mode) {
+    int xx = lower_bound(all(pt), x) - pt.begin();
+    assert(xx < size);
+    xx += size;
+    while (xx) {
+      int yy = lower_bound(all(idx[xx]), y) - idx[xx].begin();
+      assert(yy != idx[xx].size());
+      assert(y == idx[xx][yy]);
+      if (mode) st[xx].set(yy, v);
+      else st[xx].set(yy, st[xx].get(yy) + v);
+      xx >>= 1;
     }
   }
-  S prod(int sx, int tx, int sy, int ty, int k, int l, int r) {
+  S prod(i64 sx, i64 tx, i64 sy, i64 ty, int k, int l, int r) {
     assert(k < size * 2);
     assert(l < r);
-    if (r <= sx or tx <= l) return e();
-    if (sx <= l and r <= tx) {
-      int syy = lower_bound(all(index[k]), sy) - index[k].begin();
-      int tyy = lower_bound(all(index[k]), ty) - index[k].begin();
+    int sxx = lower_bound(all(pt), sx) - pt.begin();
+    int txx = lower_bound(all(pt), tx) - pt.begin();
+    if (r <= sxx or txx <= l) return e();
+    if (sxx <= l and r <= txx) {
+      int syy = lower_bound(all(idx[k]), sy) - idx[k].begin();
+      int tyy = lower_bound(all(idx[k]), ty) - idx[k].begin();
       return st[k].prod(syy, tyy);
     }
     int md = (l + r) / 2;
@@ -72,15 +87,13 @@ private:
 int main() {
   int n, q;
   cin >> n >> q;
-  pair<map<i64, int>, map<i64, int>> comp;
-  pair<vector<i64>, vector<i64>> decomp;
   vector<vector<i64>> query;
+  SegTree2d<i64, op, e> st2d;
   rep (n) {
-    vector<i64> tmp(4); tmp[0] = 0;
-    cin >> tmp[1] >> tmp[2] >> tmp[3];
-    decomp.first.push_back(tmp[1]);
-    decomp.second.push_back(tmp[2]);
-    query.push_back(tmp);
+    i64 x, y, w;
+    cin >> x >> y >> w;
+    st2d.add_point(x, y);
+    query.push_back({ 0, x, y, w });
   }
   rep (q) {
     int t;
@@ -88,41 +101,21 @@ int main() {
     if (t == 0) {
       i64 x, y, w;
       cin >> x >> y >> w;
-      decomp.first.push_back(x);
-      decomp.second.push_back(y);
+      st2d.add_point(x, y);
       query.push_back({ 0, x, y, w });
     } else {
       i64 l, d, r, u;
       cin >> l >> d >> r >> u;
-      decomp.first.push_back(l);
-      decomp.first.push_back(r);
-      decomp.second.push_back(d);
-      decomp.second.push_back(u);
       query.push_back({ 1, l, d, r, u });
     }
   }
-  // 座標圧縮
-  sort(all(decomp.first)); sort(all(decomp.second));
-  decomp.first.erase(unique(all(decomp.first)), decomp.first.end());
-  decomp.second.erase(unique(all(decomp.second)), decomp.second.end());
-  rep (decomp.first.size()) comp.first[decomp.first[i]] = i;
-  rep (decomp.second.size()) comp.second[decomp.second[i]] = i;
-
-  vector<vector<int>> idx(decomp.first.size());
-  for (vector<i64> &q: query) {
-    if (q[0]) continue;
-    idx[comp.first[q[1]]].push_back(comp.second[q[2]]);
-  }
+  st2d.init();
   // ここまで前処理
 
-  SegTree2d<i64, op, e> st2d;
-  st2d.init(idx);
   for (vector<i64> &q: query) {
-    if (q[0] == 0)
-      st2d.add(comp.first[q[1]], comp.second[q[2]], q[3]);
+    if (q[0])
+      cout << st2d.prod(q[1], q[2], q[3], q[4]) << endl;
     else
-      cout << st2d.prod(
-        comp.first[q[1]], comp.second[q[2]], comp.first[q[3]], comp.second[q[4]]
-      ) << endl;
+      st2d.add(q[1], q[2], q[3]);
   }
 }
