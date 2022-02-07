7af297b6320d24cb2be242cbe95714fcce86c0c0
lenna_kun <lennakun2469@gmail.com>
2022-02-07 23:59:18 +0900

    speed up

--- mo.cpp
+++ mo.cpp
@@ -1,7 +1,6 @@
 #include <bits/stdc++.h>
 using namespace std;
 #define i64 int64_t
-#define endl "\n"
 #define all(a) a.begin(),a.end()
 #define rep(n) for(int i = 0; i < (n); i++)
 
@@ -22,117 +21,36 @@ i64 hilbertorder(int x, int y) {
 }
 
 // S: クエリの答えのクラス
-template <
-  class S, void (*add)(int), void (*del)(int), S (*solve)()
-> struct Mo {
+template <class S> struct Mo {
 private:
-  int q;
-  vector<pair<int, int>> qs;
-  vector<int> qi;
+  int q = 0;
+  vector<int> l, r;
 public:
-  explicit Mo(vector<pair<int, int>> &q_) : q(q_.size()), qs(q_) {
-    qi.resize(q);
+  void insert(int l_, int r_) {
+    q += 1;
+    l.emplace_back(l_);
+    r.emplace_back(r_);
+  }
+
+  // F1~F5: lambda関数
+  template<typename F1, typename F2, typename F3, typename F4, typename F5>
+  vector<S> execute(F1 &&add_l, F2 &&add_r, F3 &&del_l, F4 &&del_r, F5 solve) {
+    vector<int> qi(q);
     iota(all(qi), 0);
     vector<i64> eval(q);
-    rep (q) eval[i] = hilbertorder(qs[i].first, qs[i].second);
+    rep (q) eval[i] = hilbertorder(l[i], r[i]);
     sort(all(qi), [&](int i, int j) {
       return eval[i] < eval[j];
     });
-  }
-
-  vector<S> execute() {
     vector<S> ret(q);
     int nl = 0, nr = 0;
     for (int i: qi) {
-      while (nl > qs[i].first) add(--nl);
-      while (nr < qs[i].second) add(nr++);
-      while (nl < qs[i].first) del(nl++);
-      while (nr > qs[i].second) del(--nr);
+      while (nl > l[i]) add_l(--nl);
+      while (nr < r[i]) add_r(nr++);
+      while (nl < l[i]) del_l(nl++);
+      while (nr > r[i]) del_r(--nr);
       ret[i] = solve();
     }
     return move(ret);
   }
 };
-
-struct Osa_k {
-private:
-  vector<int> min_factor;
-public:
-  Osa_k(int n){
-    //素数候補を管理する配列
-    min_factor = vector<int>(n+1);
-    //2以上の数を一度素数候補に入れる
-    rep (n+1) min_factor[i] = i;
-    //それぞれの数について最小の素因数を求める
-    for (int i=2; i*i <= n; i++){
-      if (min_factor[i] != i) continue;
-      for(int j=2; i*j <= n; j++)
-        if(min_factor[i*j] > i) min_factor[i*j] = i;
-    }
-  }
-
-  vector<pair<int, int>> prime_division(i64 n) { // O(log n)
-    vector<pair<int, int>> ret;
-    while (n > 1) {
-      if (!ret.empty() && min_factor[n] == ret.back().first)
-        ret.back().second++;
-      else
-        ret.push_back({min_factor[n], 1});
-      n /= min_factor[n];
-    }
-    return ret;
-  }
-};
-
-vector<vector<pair<int, int>>> pd;
-vector<int> prime_cnt(1000010);
-int cnt = 0;
-
-void add(int i) {
-  for (pair<int, int> &p: pd[i]) {
-    if (prime_cnt[p.first] == 0) cnt++;
-    prime_cnt[p.first] = (prime_cnt[p.first] + p.second)%3;
-    if (prime_cnt[p.first] == 0) cnt--;
-  }
-}
-
-void del(int i) {
-  for (pair<int, int> &p: pd[i]) {
-    if (prime_cnt[p.first] == 0) cnt++;
-    prime_cnt[p.first] = (prime_cnt[p.first] - p.second)%3;
-    if (prime_cnt[p.first] == 0) cnt--;
-  }
-}
-
-bool solve() {
-  return cnt == 0;
-}
-
-void Main() {
-  Osa_k osa_k(1000010);
-  int n, q;
-  cin >> n >> q;
-  pd.resize(n);
-  vector<int> a(n);
-  rep (n) {
-    cin >> a[i];
-    pd[i] = osa_k.prime_division(a[i]);
-  }
-  vector<pair<int, int>> qs(q);
-  for (pair<int, int> &e: qs) {
-    cin >> e.first >> e.second;
-    e.first--;
-  }
-  Mo<bool, add, del, solve> mo(qs);
-  for (bool ans: mo.execute()) {
-    if (ans) cout << "Yes" << endl;
-    else cout << "No" << endl;
-  }
-}
-
-int main() {
-  cin.tie(0);
-  ios::sync_with_stdio(false);
-  cout << setprecision(10) << fixed;
-  Main();
-}
